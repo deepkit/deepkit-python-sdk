25f47811b2d649128167dd12f1763be64a039b3b
Marc J. Schmidt <marc@marcjschmidt.de>
2020-03-09 23:49:38 +0100

    added better keras cifar10 model

--- examples/keras-cifar10/model.py
+++ examples/keras-cifar10/model.py
@@ -13,7 +13,7 @@ from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, Activation
 
 import deepkit
 
@@ -23,7 +23,7 @@ experiment.add_file(__file__)
 batch_size = experiment.intconfig('batch_size', 16)
 num_classes = 10
 epochs = experiment.intconfig('epochs', 15)
-data_augmentation = False
+data_augmentation = experiment.boolconfig('data_augmentation', False)
 num_predictions = 20
 
 save_dir = os.path.join(os.getcwd(), 'saved_models')
@@ -70,16 +70,27 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(12, kernel_size=(3, 3),
-                 activation='relu',
+model.add(Conv2D(32, (3, 3), padding='same',
                  input_shape=x_train.shape[1:]))
-model.add(Conv2D(16, (3, 3), activation='relu'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
 model.add(Flatten())
-model.add(Dense(32, activation='relu'))
+model.add(Dense(512))
+model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
 
 opt = keras.optimizers.Adadelta(lr=experiment.floatconfig('lr', 0.1))
 
